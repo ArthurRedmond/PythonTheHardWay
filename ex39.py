42cfbf299c39b0ed5574a91a49c60c5244c7f00b
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-11-11 20:51:55 -0500

    corrected errors in 39

--- ex39.py
+++ ex39.py
@@ -1,55 +1,55 @@
 # create a maping of state abbreviatons
-states = [
+states = {
         'Oregon': 'OR',
         'Florida': 'FL',
         'California': 'CA',
         'New York': 'NY',
         'Michigan': 'MI'
-        ]
+        }
 
 # create a basic set of states and some cities in them
-cities = [
+cities = {
         'CA': 'San Francisco',
         'MI': 'Detroit',
         'FL': 'Jacksonville'
-        ]
+        }
 
 # add some more cities
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
 # print out some cities
-print('-') * 10
+print('-' * 10)
 print("NY State has: ", cities['NY'])
 print("OR State has: ", cities['OR'])
 
 # print some states
-print('-') * 10
+print('-' * 10)
 print("Michigan's abbreviation is: ", states['Michigan'])
 print("Florida's abbreviation is: ", states['Florida'])
 
 # print every city in state
-print('-') * 10
+print('-' * 10)
 for state, abbrev in states.items():
     print("%s is abbrevated %s" % (state, abbrev))
 
 # print every state abbreviation
-print('-') * 10
+print('-' * 10)
 for abbrev, city in cities.items():
     print("%s has the city %s" % (abbrev, city))
 
 # print every city in state
-print('-') * 10
-for abbrev, city in cities.item():
-    print("%s has the city %s" % (abbrev, city))
-
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+    
 # now do both at the same time
-print('-') * 10
-for state, abbrev in states.item():
-    print("%s state is abbreviated %s and has city %s" % (
-        state, abbrev, cities[abbrev])
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
 
-print('-') * 10
+print('-' * 10)
 # safely get an abbreciation by state that might not be there
 state = states.get('Texas', None)
 
@@ -59,8 +59,4 @@ if not state:
 # get a city with a defaut value
 city = cities.get('TX', 'Does Not Exist')
 print("The city for the state 'TX' is: %s" % city)
-
-
-
-
 
